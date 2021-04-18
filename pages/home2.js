3f930bf031b560583c9f54bc8348848c86529c5e
Sirena Gandy <sirena@sirenaalyce.com>
2021-04-18 03:30:18 -0400

    changed background opacity for work example cards

--- pages/home2.js
+++ pages/home2.js
@@ -36,7 +36,7 @@ const useStyles = makeStyles({
     background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)'
   },
   card: {
-    background: 'rgba( 255, 255, 255, 0.25 )',
+    background: 'rgba( 255, 255, 255, 0.75 )',
     boxShadow: '0 8px 32px 0 rgba( 31, 38, 135, 0.37 )',
     backdropFilter: 'blur( 4px )',
     borderRadius: 10,
