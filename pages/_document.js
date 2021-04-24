ab20fa0cb0ea4f920f08f543ef6f907be5941e12
Sirena Gandy <sirena@sirenaalyce.com>
2021-04-23 20:14:18 -0400

    added google analytics

--- pages/_document.js
+++ pages/_document.js
@@ -8,6 +8,23 @@ export default class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
+           {/* Global Site Tag (gtag.js) - Google Analytics */}
+           <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_ANALYTICS_ID}`}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', '${process.env.NEXT_PUBLIC_ANALYTICS_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+            }}          
           {/* PWA primary color */}
           <meta name="theme-color" content={theme.palette.primary.main} />
           <link
