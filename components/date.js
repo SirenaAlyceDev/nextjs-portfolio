da0c19e219c4ca53e0adfd1f362874a53dd9dcc9
Sirena Gandy <sirena@sirenaalyce.com>
2021-08-12 23:27:42 -0400

    feat: date config cms

--- components/date.js
+++ components/date.js
@@ -1,6 +1,6 @@
 import { parseISO, format } from 'date-fns'
 
 export default function Date({ dateString }) {
-  const date = parseISO(JSON.stringify(dateString))
+  const date = JSON.parse(JSON.stringify(dateString))
   return <time dateTime={dateString}>{format(date, 'LLLL d, yyyy')}</time>
 }
