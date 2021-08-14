a57d2837e4f3e84b06a0d81feab686e65cb20109
Sirena Gandy <sirena@sirenaalyce.com>
2021-08-14 11:05:15 -0400

    test commit

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -32,8 +32,7 @@ export default function Post({ postData, home }) {
       <article>
         <h1>{postData.title}</h1>
         <div>
-          {'"' + JSON.parse(JSON.stringify(postData.date)) + '"'}
-          {/* <Date dateString={postData.date} /> */}
+          <Date dateString={postData.date} />
         </div>
         <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
       </article>
