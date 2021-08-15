c77551d857defb5804e4f715851f04ef1a75f6b7
Sirena Gandy <sirena@sirenaalyce.com>
2021-08-14 22:40:16 -0400

    removed time_format

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,7 +1,6 @@
 import { getAllPostIds, getPostData } from "../../lib/posts";
 import Head from "next/head";
 import Date from "../../components/date";
-import { parseISO } from "date-fns";
 import Link from "next/link";
 import { Container } from "reactstrap";
 import { DiscussionEmbed } from "disqus-react";
@@ -33,8 +32,7 @@ export default function Post({ postData, home }) {
       <article>
         <h1>{postData.title}</h1>
         <div>
-          <Date dateString={parseISO(postData.date)} />
-          {/* {postData.date} */}
+          <Date dateString={postData.date} />
         </div>
         <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
       </article>
