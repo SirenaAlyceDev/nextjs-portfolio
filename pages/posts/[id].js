074b5f294418c3a226c8ef8c3c69136ef2a89ffe
Sirena Gandy <sirena@sirenaalyce.com>
2021-08-15 00:11:11 -0400

    refract: testing cms date config

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -2,7 +2,6 @@ import { getAllPostIds, getPostData } from "../../lib/posts";
 import Head from "next/head";
 import Date from "../../components/date";
 import Link from "next/link";
-import { parseISO, format } from 'date-fns';
 import { Container } from "reactstrap";
 import { DiscussionEmbed } from "disqus-react";
 
@@ -33,7 +32,8 @@ export default function Post({ postData, home }) {
       <article>
         <h1>{postData.title}</h1>
         <div>
-          <Date dateString={parseISO(postData.date)} />
+          {/* <Date dateString={postData.date} /> */}
+          <p>{postData.date}</p>
         </div>
         <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
       </article>
