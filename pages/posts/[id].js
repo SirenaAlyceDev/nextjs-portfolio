c7dcc3b2ddf5e7c16902a297aaf51e82f4c3c46b
Sirena Gandy <sirena@sirenaalyce.com>
2021-08-14 22:36:02 -0400

    refract: parseISO() added to date

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,6 +1,7 @@
 import { getAllPostIds, getPostData } from "../../lib/posts";
 import Head from "next/head";
 import Date from "../../components/date";
+import { parseISO } from "date-fns";
 import Link from "next/link";
 import { Container } from "reactstrap";
 import { DiscussionEmbed } from "disqus-react";
@@ -32,8 +33,8 @@ export default function Post({ postData, home }) {
       <article>
         <h1>{postData.title}</h1>
         <div>
-          {/* <Date dateString={postData.date} /> */}
-          {postData.date}
+          <Date dateString={parseISO(postData.date)} />
+          {/* {postData.date} */}
         </div>
         <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
       </article>
