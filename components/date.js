fe3fbe464a894bed73d1b2bdda09a439592beeca
Sirena Gandy <sirena@sirenaalyce.com>
2021-08-24 23:33:29 -0400

    feat: added cover image configuration to netlifycms config.yml file

--- components/date.js
+++ components/date.js
@@ -1,10 +1,8 @@
-import { parseISO, format } from 'date-fns';
 import moment from 'moment';
 import superjson from 'superjson';
 
 export default function Date({ dateString }) {
   const date = superjson.parse(dateString)
-  // return <time dateTime={dateString}>{format(date, 'LLLL d, yyyy')}</time>
   return <time dateTime={dateString}>{moment(date).format('LL')}</time>
 
 }
